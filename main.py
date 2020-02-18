df61427141fc27e49577c42710382f04b3445acf
attops <wang6hai@gmail.com>
2020-02-18 19:59:12 +0800

    yaml tune

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def read_file(file_path):
     with open(file_path) as f:
         content = f.readlines()
@@ -7,13 +10,13 @@ def read_file(file_path):
 
 
 def write_file(file_path, content):
-    f = open(file_path, 'w')
+    f = open(file_path, 'a')
     f.write(content)
     f.close()
 
 
 if __name__ == '__main__':
+    print('np.version.full_version : ' + np.version.full_version)
     nums = read_file('/var/nfs/read/number_to_add.txt')
     write_file('/var/nfs/write/number_added.txt', str(nums[0] + nums[1]))
-    # nums = read_file('./tmp_files/r.txt')
-    # write_file('./tmp_files/w.txt', str(nums[0] + nums[1]))
+    write_file('/var/nfs/write/number_added.txt', str(np.arange(6)))
